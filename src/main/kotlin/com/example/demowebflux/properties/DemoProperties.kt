386f70009c19a31cb4ad9deb2a873702ef41dba8
Fornitx <49841725+Fornitx@users.noreply.github.com>
2023-08-03 23:25:50 +0300

    WebClient timeouts

--- src/main/kotlin/com/example/demowebflux/properties/DemoProperties.kt
+++ src/main/kotlin/com/example/demowebflux/properties/DemoProperties.kt
@@ -3,8 +3,11 @@ package com.example.demowebflux.properties
 import com.example.demowebflux.constants.PREFIX
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.time.DurationMax
+import org.hibernate.validator.constraints.time.DurationMin
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.validation.annotation.Validated
+import java.time.Duration
 
 @ConfigurationProperties(PREFIX, ignoreUnknownFields = false)
 @Validated
@@ -16,12 +19,29 @@ data class DemoProperties(
 ) {
     data class ServiceProperties(
         val cache: Boolean,
+
         @field:Min(1)
         val multiplier: Int,
     )
 
     data class ClientProperties(
         @field:NotBlank
-        val url: String
+        val url: String,
+
+        @field:DurationMin(millis = 100)
+        @field:DurationMax(seconds = 60)
+        val responseTimeout: Duration,
+
+        @field:DurationMin(millis = 100)
+        @field:DurationMax(seconds = 60)
+        val connectionTimeout: Duration,
+
+        @field:DurationMin(millis = 100)
+        @field:DurationMax(seconds = 60)
+        val readTimeout: Duration,
+
+        @field:DurationMin(millis = 100)
+        @field:DurationMax(seconds = 60)
+        val writeTimeout: Duration,
     )
 }
--- src/main/kotlin/com/example/demowebflux/rest/client/DemoClientImpl.kt
+++ src/main/kotlin/com/example/demowebflux/rest/client/DemoClientImpl.kt
@@ -1,14 +1,33 @@
 package com.example.demowebflux.rest.client
 
 import com.example.demowebflux.properties.DemoProperties
+import io.netty.channel.ChannelOption
+import io.netty.handler.timeout.ReadTimeoutHandler
+import io.netty.handler.timeout.WriteTimeoutHandler
 import org.springframework.http.ResponseEntity
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitEntity
 import org.springframework.web.reactive.function.client.awaitExchange
+import reactor.netty.http.client.HttpClient
+import java.util.concurrent.TimeUnit
 
 class DemoClientImpl(private val properties: DemoProperties.ClientProperties) : DemoClient {
-    private val client = WebClient.create(properties.url)
+    private val client = WebClient.builder()
+        .baseUrl(properties.url)
+        .clientConnector(
+            ReactorClientHttpConnector(
+                HttpClient.create()
+                    .responseTimeout(properties.responseTimeout)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, properties.connectionTimeout.toMillis().toInt())
+                    .doOnConnected { conn ->
+                        conn.addHandler(ReadTimeoutHandler(properties.readTimeout.toMillis(), TimeUnit.MILLISECONDS))
+                        conn.addHandler(WriteTimeoutHandler(properties.writeTimeout.toMillis(), TimeUnit.MILLISECONDS))
+                    }
+            )
+        )
+        .build()
 
     override suspend fun call(msg: String): String {
         return client.post()
