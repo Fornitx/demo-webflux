cd2b9fe71b059fb272d6de4232e02ce83c133fd1
Fornitx <49841725+Fornitx@users.noreply.github.com>
2023-12-01 19:44:44 +0300

    management

--- src/test/kotlin/com/example/demowebflux/OpenApiTest.kt
+++ src/test/kotlin/com/example/demowebflux/OpenApiTest.kt
@@ -1,5 +1,6 @@
 package com.example.demowebflux
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.EnabledOnOs
 import org.junit.jupiter.api.condition.OS
 import org.springframework.boot.test.context.SpringBootTest
@@ -8,7 +9,7 @@ import java.util.concurrent.TimeUnit
 @EnabledOnOs(OS.WINDOWS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class OpenApiTest {
-//    @Test
+    @Test
     fun test() {
         TimeUnit.HOURS.sleep(1)
     }
