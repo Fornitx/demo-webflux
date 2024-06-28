f40e61c4a9f57d53eba78b90399d2a45e70b4737
Fornitx <49841725+Fornitx@users.noreply.github.com>
2024-06-28 19:40:07 +0300

    settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("org.springframework.boot") version System.getProperty("spring_version")
-    id("io.spring.dependency-management") version System.getProperty("spring_dm_version")
-    kotlin("jvm") version System.getProperty("kotlin_version")
-    kotlin("plugin.spring") version System.getProperty("kotlin_version")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
 }
 
 group = "com.example"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    plugins {
+        id("org.springframework.boot") version System.getProperty("spring_version")
+        id("io.spring.dependency-management") version System.getProperty("spring_dm_version")
+        kotlin("jvm") version System.getProperty("kotlin_version")
+        kotlin("plugin.spring") version System.getProperty("kotlin_version")
+    }
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
