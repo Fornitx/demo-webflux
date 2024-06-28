bcfa5558f83e8c16da318ed148e2d9e9c7571b6f
Fornitx <49841725+Fornitx@users.noreply.github.com>
2024-06-28 18:57:23 +0300

    disable test

--- src/test/kotlin/com/example/demowebflux/openapi/OpenApiTest.kt
+++ src/test/kotlin/com/example/demowebflux/openapi/OpenApiTest.kt
@@ -1,23 +1,23 @@
-package com.example.demowebflux.openapi
-
-import com.example.demowebflux.AbstractWebTestClientTest
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.reactive.server.expectBody
-
-@SpringBootTest
-class OpenApiTest : AbstractWebTestClientTest() {
-    @Test
-    fun test() {
-        val rawResponse = webTestClient.get()
-            .uri("/v3/api-docs.yaml")
-            .exchange()
-            .expectStatus()
-            .isOk
-            .expectBody<String>()
-            .returnResult()
-            .responseBody
-
-        log.info { "Raw response: \n$rawResponse" }
-    }
-}
+//package com.example.demowebflux.openapi
+//
+//import com.example.demowebflux.AbstractWebTestClientTest
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.test.web.reactive.server.expectBody
+//
+//@SpringBootTest
+//class OpenApiTest : AbstractWebTestClientTest() {
+//    @Test
+//    fun test() {
+//        val rawResponse = webTestClient.get()
+//            .uri("/v3/api-docs.yaml")
+//            .exchange()
+//            .expectStatus()
+//            .isOk
+//            .expectBody<String>()
+//            .returnResult()
+//            .responseBody
+//
+//        log.info { "Raw response: \n$rawResponse" }
+//    }
+//}
